b463e80093be6cc88026d2623e5c50e71429db71
me-naresh141 <me.naresh141@gmail.com>
2022-08-11 16:26:55 +0530

    this is yoga website

---
+++ index.js
@@ -0,0 +1,41 @@
+let section = document.querySelector('.data-section')
+let num = document.querySelector('.count')
+let value = 0
+let workshop = document.querySelector('.total-workshop')
+let year = document.querySelector('.total-year')
+let lesson = document.querySelector('.total-lesson')
+
+section.addEventListener('mouseover', () => {
+  for (let i = 1; i <= 5000; i++) {
+    let id = setInterval(() => {
+      if (num.innerText == 5000) {
+        clearInterval(id)
+      } else {
+        console.dir(value)
+        value = i
+        num.innerText = value
+      }
+      // 2
+      if (workshop.innerText == 4000) {
+        clearInterval(id)
+      } else {
+        value = i
+        workshop.innerText = value
+      }
+      // 3
+      if (year.innerText == 570) {
+        clearInterval(id)
+      } else {
+        value = i
+        year.innerText = value
+      }
+      // 4
+      if (lesson.innerText == 900) {
+        clearInterval(id)
+      } else {
+        value = i
+        lesson.innerText = value
+      }
+    }, 400)
+  }
+})
